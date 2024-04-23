8ae9174b074c7fe64e2ae9093b96c208d83fab72
Vlad Motoloev <99215656+vlad-fallen@users.noreply.github.com>
2024-04-23 14:01:12 +0300

    Delete ApiVersion directory

--- ApiVersion/DTO/v1/RestDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KinopoiskApiVersion.DTO.v1
-{
-    public class RestDTO<T>
-    {
-        public List<LinkDTO> Links { get; set; } = new List<LinkDTO>();
-
-        public T Data { get; set; } = default!;
-    }
-}
--- ApiVersion/DTO/v2/RestDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KinopoiskApiVersion.DTO.v2
-{
-    public class RestDTO<T>
-    {
-        public List<v1.LinkDTO> Links { get; set; } = new List<v1.LinkDTO>();
-
-        public T Items { get; set; } = default!;
-    }
-}
